e6feca7a8f058f260e2c7728e0f13d1e48750537
Auroiah Morgan <amwebdev@outlook.com>
2021-10-13 15:15:40 -0400

    Update WeaponManager.cs

--- Assets/Game/Scripts/Player/Weapon/WeaponManager.cs
+++ Assets/Game/Scripts/Player/Weapon/WeaponManager.cs
@@ -8,9 +8,9 @@ public class WeaponManager : MonoBehaviour
   [SerializeField] int primaryAmmo;
   [SerializeField] List<SOWeapon> weaponInventory;
   [SerializeField] Camera fpsCam;
-  SOWeapon primarySlot1 = null;
+  [SerializeField] SOWeapon primarySlot1 = null;
   SOWeapon primarySlot2 = null;
-  SOWeapon secondarySlot1 = null;
+  [SerializeField] SOWeapon secondarySlot1 = null;
   SOWeapon currentWeapon = null;
 
 
@@ -20,10 +20,24 @@ public class WeaponManager : MonoBehaviour
     fpsCam = GetComponentInChildren<Camera>();
 
   }
-
+  /// <summary>
+  /// Adds the weapon to the Player's inventory on pickup. Slots the item in the appropriate slot if the slots are null.
+  /// </summary>
+  /// <param name="weapon">The weapon to add to the player's inventory</param>
   public void AddWeaponToInventory(SOWeapon weapon)
   {
     weaponInventory.Add(weapon);
+    if (weapon.WeaponType == WeaponType.Primary && primarySlot1 == null)
+    {
+      EquipPrimary(weapon);
+    }
+    else if (weapon.WeaponType == WeaponType.Secondary && secondarySlot1 == null)
+    {
+      EquipSecondary(weapon);
+    }
+    print("The list has " + weaponInventory[0]);
+    if (primarySlot1 == null) print("Primary is null");
+    // print("Added weapon " + primarySlot1.name);
   }
 
   public bool EquipWeapon(SOWeapon weapon)
@@ -61,6 +75,7 @@ public class WeaponManager : MonoBehaviour
     if (primarySlot1 == null)
     {
       primarySlot1 = weapon;
+      print("Equipped to slot 1");
       return true;
     }
     else if (primarySlot2 == null)
