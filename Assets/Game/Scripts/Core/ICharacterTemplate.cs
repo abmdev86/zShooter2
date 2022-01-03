6c34c09664e710372968a69331112afd3c55895a
Auroiah Morgan <amwebdev@outlook.com>
2022-01-03 01:36:57 -0500

    Create ICharacterTemplate.cs

---
+++ Assets/Game/Scripts/Core/ICharacterTemplate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.sluggaGames.gambit.Core
+{
+  public interface ICharacterTemplate
+  {
+    void CharacterStats(SOCharacter character);
+  }
+}
