ba26649d61959c86e539193ba4a745f94a219323
Auroiah Morgan <amwebdev@outlook.com>
2022-01-03 01:37:02 -0500

    Update PlayerSpawner.cs

--- Assets/Game/Scripts/Core/PlayerSpawner.cs
+++ Assets/Game/Scripts/Core/PlayerSpawner.cs
@@ -1,5 +1,5 @@
-
 using UnityEngine;
+using com.sluggaGames.gambit.PlayerSystem;
 
 namespace com.sluggaGames.gambit.Core
 {
@@ -7,15 +7,23 @@ namespace com.sluggaGames.gambit.Core
   {
     SOCharacter characterData;
     GameObject player;
+    [SerializeField]
+    GameObject spawnLocation;
 
     void Start()
     {
       CreatePlayerFromData();
     }
+    void SetPlayer()
+    {
+      player.transform.SetParent(this.transform);
+      player.transform.position = spawnLocation.transform.position;
+    }
     void CreatePlayerFromData()
     {
       characterData = Object.Instantiate(Resources.Load("Data/Player/Player_Default")) as SOCharacter;
       player = GameObject.Instantiate(characterData.Model) as GameObject;
+      player.GetComponent<Player>().CharacterStats(characterData);
     }
   }
 }
